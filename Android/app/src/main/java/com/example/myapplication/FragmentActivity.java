f4d260c2b226ef292d9dc5e13584df6e255b3caf
KhoaHN612 <khoahnvithuy@gmail.com>
2022-10-17 16:19:00 +0700

    Update Bot Nav Frag

--- Android/app/src/main/java/com/example/myapplication/FragmentActivity.java
+++ Android/app/src/main/java/com/example/myapplication/FragmentActivity.java
@@ -1,10 +1,14 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+
 public class FragmentActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
