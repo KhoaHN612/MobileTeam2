7a33a73161db990dcb8bdb2241aec0850a46a5eb
KhoaHN612 <khoahnvithuy@gmail.com>
2022-11-11 11:29:45 +0700

    BookManageAdd

---
+++ BookManage/app/src/main/java/com/example/bookmanage/AddActivity.java
@@ -0,0 +1,33 @@
+package com.example.bookmanage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddActivity extends AppCompatActivity {
+
+    EditText title_input, author_input, page_input;
+    Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        title_input = findViewById(R.id.title_input);
+        author_input = findViewById(R.id.author_input);
+        page_input = findViewById(R.id.page_input);
+
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MyDatabaseHelper myDB = new MyDatabaseHelper(AddActivity.this);
+            }
+        })
+    }
+}
---
+++ BookManage/app/src/main/java/com/example/bookmanage/MyDatabaseHelper.java
@@ -0,0 +1,60 @@
+package com.example.bookmanage;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DB_NAME = "BookLibrary.db";
+    private static final int DB_VERSION = 1;
+
+    private static final String TABLE_NAME = "my_library";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "book_title";
+    private static final String COLUMN_AUTHOR = "book_author";
+    private static final String COLUMN_PAGES = "book_pages";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query =
+                "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + "INTERGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_AUTHOR + " TEXT, " +
+                        COLUMN_PAGES + " INTERGER);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    void addBook (String title, String author, int pages){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_AUTHOR, author);
+        cv.put(COLUMN_PAGES, pages);
+
+        long result = db.insert(TABLE_NAME, null, cv);
+        if (result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Added Successfully!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
